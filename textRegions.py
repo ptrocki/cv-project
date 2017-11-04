113564ac703a785158050b712993f46a11e9e90e
Piotr Trocki <piotr@local.pl>
2017-11-04 19:41:45 +0100

    First concept

--- textRegions.py
+++ textRegions.py
@@ -2,59 +2,77 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('sc.jpeg')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+def trocki():
+    img = cv2.imread('answers.png')
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    mser = cv2.MSER_create(_max_area=120)
+    vis = img.copy()
+    regions, bboxes = mser.detectRegions(gray)
+    # hulls = [cv2.convexHull(p.reshape(-1, 1, 2)) for p in regions]
+    # cv2.polylines(vis, hulls, 1, (0, 255, 0))
+    xmin = bboxes[:, 0]
+    ymin = bboxes[:, 1]
 
+    smallestx = int(round(min(xmin), 0))
+    smallesty = int(round(min(ymin), 0))
 
-mser = cv2.MSER_create()
-# mser.setMinArea(100)
-# mser.setMaxArea(800)
+    cv2.rectangle(vis, (smallestx, smallesty), (smallestx + 20, smallesty + 20), (0, 255, 0), 2)
+    cv2.imwrite("answers_result.png", vis)
 
-coordinates, bboxes = mser.detectRegions(gray)
-bboxes2 = bboxes.copy()
+def barber():
+    img = cv2.imread('sc.jpeg')
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    mser = cv2.MSER_create()
+    # mser.setMinArea(100)
+    # mser.setMaxArea(800)
 
-imgcopy = img.copy()
+    coordinates, bboxes = mser.detectRegions(gray)
+    bboxes2 = bboxes.copy()
 
-xmin = bboxes[:, 0]
-ymin = bboxes[:, 1]
-xmax = bboxes[:, 2]
-ymax = bboxes[:, 3]
+    imgcopy = img.copy()
 
-avgHeight = sum(ymax) / len(ymax)
+    xmin = bboxes[:, 0]
+    ymin = bboxes[:, 1]
+    xmax = bboxes[:, 2]
+    ymax = bboxes[:, 3]
 
-xmax += xmin[:] - 1
-ymax += ymin[:] - 1
+    avgHeight = sum(ymax) / len(ymax)
 
-expansionAmount = 0.02
-xmin = (1-expansionAmount) * xmin
-ymin = (1-expansionAmount) * ymin
-xmax = (1+expansionAmount) * xmax
-ymax = (1+expansionAmount) * ymax
+    xmax += xmin[:] - 1
+    ymax += ymin[:] - 1
 
-smallestx = int(round(min(xmin), 0))
-smallesty = int(round(min(ymin), 0))
-biggestx = int(round(max(xmax), 0))
-biggesty = int(round(max(ymax), 0))
+    expansionAmount = 0.02
+    xmin = (1-expansionAmount) * xmin
+    ymin = (1-expansionAmount) * ymin
+    xmax = (1+expansionAmount) * xmax
+    ymax = (1+expansionAmount) * ymax
 
-cv2.rectangle(imgcopy, (smallestx, smallesty), (biggestx, biggesty), (0, 0, 0), 2)
+    smallestx = int(round(min(xmin), 0))
+    smallesty = int(round(min(ymin), 0))
+    biggestx = int(round(max(xmax), 0))
+    biggesty = int(round(max(ymax), 0))
 
-cv2.imwrite("result1.png", imgcopy)
+    cv2.rectangle(imgcopy, (smallestx, smallesty), (biggestx, biggesty), (0, 0, 0), 2)
 
-rectangle = [smallestx, smallesty, smallestx, smallesty]
-rectangles = []
+    cv2.imwrite("result1.png", imgcopy)
 
-#needa work out smth better, bbox aren't sorted, so this doesn't work
-for bb in bboxes2:
-    if (rectangle[3] - bb[1]) > avgHeight:
-        rectangles.append(rectangle)
-        rectangle = [bb[0], bb[1], bb[0] + bb[2], bb[1] + bb[3]]
-    else:
-        rectangle[0] = int(round(min([rectangle[0], bb[0]])))
-        rectangle[1] = int(round(min([rectangle[1], bb[1]])))
-        rectangle[2] = int(round(max([rectangle[2], bb[0] + bb[2]])))
-        rectangle[3] = int(round(max([rectangle[3], bb[1] + bb[3]])))
+    rectangle = [smallestx, smallesty, smallestx, smallesty]
+    rectangles = []
 
-for rect in bboxes:
-    cv2.rectangle(imgcopy, (rect[0], rect[1]), (rect[2], rect[3]), (0, 0, 0), 1)
+    #needa work out smth better, bbox aren't sorted, so this doesn't work
+    for bb in bboxes2:
+        if (rectangle[3] - bb[1]) > avgHeight:
+            rectangles.append(rectangle)
+            rectangle = [bb[0], bb[1], bb[0] + bb[2], bb[1] + bb[3]]
+        else:
+            rectangle[0] = int( round(min([rectangle[0], bb[0]])))
+            rectangle[1] = int(round(min([rectangle[1], bb[1]])))
+            rectangle[2] = int(round(max([rectangle[2], bb[0] + bb[2]])))
+            rectangle[3] = int(round(max([rectangle[3], bb[1] + bb[3]])))
 
-cv2.imwrite("result2.png", imgcopy)
+    for rect in bboxes:
+        cv2.rectangle(imgcopy, (rect[0], rect[1]), (rect[2], rect[3]), (0, 0, 0), 1)
+
+    cv2.imwrite("result2.png", imgcopy)
+
+trocki()
