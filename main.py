d350fba66ad9821a5acca3050f5e8d6e03286660
Fryzjerro <lukasz@karnickiisyn.pl>
2017-11-15 19:46:19 +0100

    Refactor

---
+++ main.py
@@ -0,0 +1,6 @@
+
+import numpy as np
+import cv2
+import textRegions as txt
+
+txt.TextRegions.findText('input/test.jpg')
--- textRegions.py
+++ textRegions.py
@@ -4,244 +4,252 @@ import math
 import numpy as np
 
 
-def apprendRectangle(box, rectangle):
-    if box[0] < rectangle[0]:
-        rectangle[0] = box[0]
+class TextRegions:
 
-    if box[1] < rectangle[1]:
-        rectangle[1] = box[1]
+    @staticmethod
+    def apprendRectangle(box, rectangle):
+        if box[0] < rectangle[0]:
+            rectangle[0] = box[0]
 
-    x = box[0] + box[2]
-    y = box[1] + box[3]
-    if x > rectangle[2]:
-        rectangle[2] = x
+        if box[1] < rectangle[1]:
+            rectangle[1] = box[1]
 
-    if y > rectangle[3]:
-        rectangle[3] = y
+        x = box[0] + box[2]
+        y = box[1] + box[3]
+        if x > rectangle[2]:
+            rectangle[2] = x
 
-    return rectangle
+        if y > rectangle[3]:
+            rectangle[3] = y
 
+        return rectangle
 
-def mergeTwoBoxes(box1, box2):
-    if box1[0] <= box2[0] and box1[2] + box1[0] > box2[2] + box2[0]:
-        x1 = box1[0]
-        x2 = box1[2]
-    elif box1[0] <= box2[0]:
-        x1 = box1[0]
-        x2 = box2[0] - box1[0] + box2[2]
-    else:
-        x1 = box2[0]
-        x2 = box1[0] - box2[0] + box1[2]
+    @staticmethod
+    def mergeTwoBoxes(box1, box2):
+        if box1[0] <= box2[0] and box1[2] + box1[0] > box2[2] + box2[0]:
+            x1 = box1[0]
+            x2 = box1[2]
+        elif box1[0] <= box2[0]:
+            x1 = box1[0]
+            x2 = box2[0] - box1[0] + box2[2]
+        else:
+            x1 = box2[0]
+            x2 = box1[0] - box2[0] + box1[2]
 
-    if box1[1] <= box2[1] and box1[3] + box1[1] > box2[3] + box2[1]:
-        y1 = box1[1]
-        y2 = box1[3]
-    elif box1[1] <= box2[1]:
-        y1 = box1[1]
-        y2 = box2[1] - box1[1] + box2[3]
-    else:
-        y1 = box2[1]
-        y2 = box1[1] - box2[1] + box1[3]
+        if box1[1] <= box2[1] and box1[3] + box1[1] > box2[3] + box2[1]:
+            y1 = box1[1]
+            y2 = box1[3]
+        elif box1[1] <= box2[1]:
+            y1 = box1[1]
+            y2 = box2[1] - box1[1] + box2[3]
+        else:
+            y1 = box2[1]
+            y2 = box1[1] - box2[1] + box1[3]
 
-    bigBox = [x1, y1, x2, y2]
+        bigBox = [x1, y1, x2, y2]
 
-    return bigBox
+        return bigBox
 
+    @staticmethod
+    def mergeWithinHeight(bboxes):
+        new = bboxes
+        result = []
 
-def mergeWithinHeight(bboxes):
-    new = bboxes
-    result = []
+        i = 0
+        while i < len(new):
+            j = i + 1
+            bb = new[i]
+            toRemove = []
 
-    i = 0
-    while i < len(new):
-        j = i + 1
-        bb = new[i]
-        toRemove = []
+            while j < len(new):
+                bb2 = new[j]
+                isSimilar = bb[1] <= bb2[1] <= bb[1] + bb[3]
+                isSimilar = isSimilar or bb2[1] <= bb[1] <= bb2[1] + bb2[3]
 
-        while j < len(new):
-            bb2 = new[j]
-            isSimilar = bb[1] <= bb2[1] <= bb[1] + bb[3]
-            isSimilar = isSimilar or bb2[1] <= bb[1] <= bb2[1] + bb2[3]
-
-            if isSimilar:
-                bb = mergeTwoBoxes(bb, bb2)
-                toRemove.append(j)
-
-            j += 1
-
-        for j in sorted(toRemove, reverse=True):
-            new = np.delete(new, j, axis=0)
-
-            if j < i:
-                i -= 1
-
-        result.append(bb)
-        i += 1
-
-    return result
-
-
-def mergeAmbiguous(bboxes, ambiguityThreshold = 4):
-    new = bboxes
-    result = []
-
-    i = 0
-    while i < len(new):
-        j = i + 1
-        bb = new[i]
-        toRemove = []
-
-        while j < len(new):
-            bb2 = new[j]
-            isSimilar = abs(bb[0] - bb2[0]) < ambiguityThreshold
-            isSimilar = isSimilar and abs(bb[1] - bb2[1]) < ambiguityThreshold
-
-            if isSimilar:
-                bb = mergeTwoBoxes(bb, bb2)
-                toRemove.append(j)
-
-            j += 1
-
-        for j in sorted(toRemove, reverse=True):
-            new = np.delete(new, j, axis=0)
-
-            if j < i:
-                i -= 1
-
-        result.append(bb)
-        i += 1
-
-    return result
-
-
-def removeAmbiguous(bboxes, ambiguityThreshold = 3):
-    new = list(bboxes)
-    toRemove = []
-
-    i = 0
-    while i < len(new):
-        j = i + 1
-        bb = new[i]
-
-        while j < len(new):
-            bb2 = new[j]
-            isSimilar = abs(bb[0] - bb2[0]) < ambiguityThreshold
-            isSimilar = isSimilar and abs(bb[1] - bb2[1]) < ambiguityThreshold
-            isSimilar = isSimilar and abs(bb[2] - bb2[2]) < ambiguityThreshold
-            isSimilar = isSimilar and abs(bb[3] - bb2[3]) < ambiguityThreshold
-
-            if isSimilar:
-                try:
-                    toRemove.index(j)
-                except ValueError:
+                if isSimilar:
+                    bb = TextRegions.mergeTwoBoxes(bb, bb2)
                     toRemove.append(j)
 
-            j += 1
+                j += 1
 
-        i += 1
+            for j in sorted(toRemove, reverse=True):
+                new = np.delete(new, j, axis=0)
 
-    for j in sorted(toRemove, reverse=True):
-        new.pop(j)
+                if j < i:
+                    i -= 1
 
-    return new
+            result.append(bb)
+            i += 1
 
+        return result
 
-def mergeBoxes(bboxes, avgHeight):
-    rectangles = []
-    unusedBoxes = list(bboxes)
-    i = 0
+    @staticmethod
+    def mergeAmbiguous(bboxes, ambiguityThreshold = 4):
+        new = bboxes
+        result = []
 
-    while i < len(unusedBoxes):
-        box = unusedBoxes[i]
-        rectangle = [box[0], box[1], box[0] + box[2], box[1] + box[3]]
-        unused2 = unusedBoxes[:]
+        i = 0
+        while i < len(new):
+            j = i + 1
+            bb = new[i]
+            toRemove = []
+
+            while j < len(new):
+                bb2 = new[j]
+                isSimilar = abs(bb[0] - bb2[0]) < ambiguityThreshold
+                isSimilar = isSimilar and abs(bb[1] - bb2[1]) < ambiguityThreshold
+
+                if isSimilar:
+                    bb = mergeTwoBoxes(bb, bb2)
+                    toRemove.append(j)
+
+                j += 1
+
+            for j in sorted(toRemove, reverse=True):
+                new = np.delete(new, j, axis=0)
+
+                if j < i:
+                    i -= 1
+
+            result.append(bb)
+            i += 1
+
+        return result
+
+    @staticmethod
+    def removeAmbiguous(bboxes, ambiguityThreshold = 3):
+        new = list(bboxes)
         toRemove = []
 
-        for j in range(len(unused2)):
-            bb = unused2[j]
-            middlex = bb[0] + bb[2] / 2
-            middley = bb[1] + bb[3] / 2
-            corner1 = math.sqrt(pow(middlex - rectangle[0], 2) + pow(middley - rectangle[1], 2)) < avgHeight
-            corner2 = math.sqrt(pow(middlex - rectangle[2], 2) + pow(middley - rectangle[1], 2)) < avgHeight
-            corner3 = math.sqrt(pow(middlex - rectangle[2], 2) + pow(middley - rectangle[3], 2)) < avgHeight
-            corner4 = math.sqrt(pow(middlex - rectangle[0], 2) + pow(middley - rectangle[3], 2)) < avgHeight
+        i = 0
+        while i < len(new):
+            j = i + 1
+            bb = new[i]
 
-            if corner1 or corner2 or corner3 or corner4:
-                rectangle = apprendRectangle(bb, rectangle)
-                toRemove.append(j)
+            while j < len(new):
+                bb2 = new[j]
+                isSimilar = abs(bb[0] - bb2[0]) < ambiguityThreshold
+                isSimilar = isSimilar and abs(bb[1] - bb2[1]) < ambiguityThreshold
+                isSimilar = isSimilar and abs(bb[2] - bb2[2]) < ambiguityThreshold
+                isSimilar = isSimilar and abs(bb[3] - bb2[3]) < ambiguityThreshold
+
+                if isSimilar:
+                    try:
+                        toRemove.index(j)
+                    except ValueError:
+                        toRemove.append(j)
+
+                j += 1
+
+            i += 1
 
         for j in sorted(toRemove, reverse=True):
-            unusedBoxes.pop(j)
+            new.pop(j)
 
-            if j < i:
-                i -= 1
+        return new
 
-        rectangles.append(rectangle)
+    @staticmethod
+    def mergeBoxes(bboxes, avgHeight):
+        rectangles = []
+        unusedBoxes = list(bboxes)
+        i = 0
 
-        if len(unusedBoxes) == 0:
-            break
+        while i < len(unusedBoxes):
+            box = unusedBoxes[i]
+            rectangle = [box[0], box[1], box[0] + box[2], box[1] + box[3]]
+            unused2 = unusedBoxes[:]
+            toRemove = []
 
-        i += 1
+            for j in range(len(unused2)):
+                bb = unused2[j]
+                middlex = bb[0] + bb[2] / 2
+                middley = bb[1] + bb[3] / 2
+                corner1 = math.sqrt(pow(middlex - rectangle[0], 2) + pow(middley - rectangle[1], 2)) < avgHeight
+                corner2 = math.sqrt(pow(middlex - rectangle[2], 2) + pow(middley - rectangle[1], 2)) < avgHeight
+                corner3 = math.sqrt(pow(middlex - rectangle[2], 2) + pow(middley - rectangle[3], 2)) < avgHeight
+                corner4 = math.sqrt(pow(middlex - rectangle[0], 2) + pow(middley - rectangle[3], 2)) < avgHeight
 
-    return rectangles
+                if corner1 or corner2 or corner3 or corner4:
+                    rectangle = TextRegions.apprendRectangle(bb, rectangle)
+                    toRemove.append(j)
 
+            for j in sorted(toRemove, reverse=True):
+                unusedBoxes.pop(j)
 
-def convertToXYWH(toConvert):
-    result = []
+                if j < i:
+                    i -= 1
 
-    for bb in toConvert:
-        single = [bb[0], bb[1], bb[2] - bb[0], bb[3] - bb[1]]
-        result.append(single)
+            rectangles.append(rectangle)
 
-    return result
+            if len(unusedBoxes) == 0:
+                break
 
+            i += 1
 
-img = cv2.imread('test.jpg')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        return rectangles
 
-mser = cv2.MSER_create()
+    @staticmethod
+    def convertToXYWH(toConvert):
+        result = []
 
-coordinates, bboxes = mser.detectRegions(gray)
-bboxes2 = bboxes.copy()
+        for bb in toConvert:
+            single = [bb[0], bb[1], bb[2] - bb[0], bb[3] - bb[1]]
+            result.append(single)
 
-imgcopy = img.copy()
+        return result
 
-xmin = bboxes[:, 0]
-ymin = bboxes[:, 1]
-xmax = bboxes[:, 2]
-ymax = bboxes[:, 3]
+    @staticmethod
+    def findText(filename):
+        img = cv2.imread(filename)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-avgHeight = sum(ymax) / len(ymax) + 15
+        mser = cv2.MSER_create()
 
-xmax += xmin[:] - 1
-ymax += ymin[:] - 1
+        coordinates, bboxes = mser.detectRegions(gray)
+        bboxes2 = bboxes.copy()
 
-expansionAmount = 0.02
-xmin = (1-expansionAmount) * xmin
-ymin = (1-expansionAmount) * ymin
-xmax = (1+expansionAmount) * xmax
-ymax = (1+expansionAmount) * ymax
+        imgcopy = img.copy()
 
-smallestx = int(round(min(xmin), 0))
-smallesty = int(round(min(ymin), 0))
-biggestx = int(round(max(xmax), 0))
-biggesty = int(round(max(ymax), 0))
+        xmin = bboxes[:, 0]
+        ymin = bboxes[:, 1]
+        xmax = bboxes[:, 2]
+        ymax = bboxes[:, 3]
 
-cv2.rectangle(imgcopy, (smallestx, smallesty), (biggestx, biggesty), (0, 0, 0), 2)
+        avgHeight = sum(ymax) / len(ymax) + 15
 
-cv2.imwrite("result1.png", imgcopy)
+        xmax += xmin[:] - 1
+        ymax += ymin[:] - 1
 
-# rectangles = mergeAmbiguous(bboxes2)
-# rectangles = sorted(rectangles)
-rectangles = mergeBoxes(bboxes2, avgHeight)
-rectangles = convertToXYWH(rectangles)
-rectangles = mergeWithinHeight(rectangles)
-# rectangles = mergeBoxes(rectangles, avgHeight)
+        expansionAmount = 0.02
+        xmin = (1-expansionAmount) * xmin
+        ymin = (1-expansionAmount) * ymin
+        xmax = (1+expansionAmount) * xmax
+        ymax = (1+expansionAmount) * ymax
 
-for rect in rectangles:
-    # cv2.rectangle(imgcopy, (rect[0], rect[1]), (rect[2], rect[3]), (0, 0, 0), 1)
-    cv2.rectangle(imgcopy, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 0, 0), 1)
+        smallestx = int(round(min(xmin), 0))
+        smallesty = int(round(min(ymin), 0))
+        biggestx = int(round(max(xmax), 0))
+        biggesty = int(round(max(ymax), 0))
 
-cv2.imwrite("result3.png", imgcopy)
+        cv2.rectangle(imgcopy, (smallestx, smallesty), (biggestx, biggesty), (0, 0, 0), 2)
+
+        wholeRegion = (smallestx, smallesty, biggestx, biggesty)
+
+        cv2.imwrite("output/result1.png", imgcopy)
+
+        # rectangles = mergeAmbiguous(bboxes2)
+        # rectangles = sorted(rectangles)
+        rectangles = TextRegions.mergeBoxes(bboxes2, avgHeight)
+        rectangles = TextRegions.convertToXYWH(rectangles)
+        rectangles = TextRegions.mergeWithinHeight(rectangles)
+        # rectangles = mergeBoxes(rectangles, avgHeight)
+
+        for rect in rectangles:
+            # cv2.rectangle(imgcopy, (rect[0], rect[1]), (rect[2], rect[3]), (0, 0, 0), 1)
+            cv2.rectangle(imgcopy, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 0, 0), 1)
+
+        cv2.imwrite("output/result3.png", imgcopy)
+
+        return wholeRegion, rectangles
 
 
